7eed243b0657a5435b8eabf21a66c2a9c7b6e328
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-08-24 12:31:09 +0530

    update footer and main information

--- src/assets/configs/footerConfig.js
+++ src/assets/configs/footerConfig.js
@@ -1,32 +1,38 @@
 import {AiFillGithub} from "react-icons/ai";
-import {FaLinkedinIn} from "react-icons/fa";
-import {BsMedium} from "react-icons/bs";
+import { FaLinkedinIn, FaKaggle, FaGoodreadsG } from "react-icons/fa";
 import React from "react";
 
 const footerConfig = {
-    icons: [
-        {
-            id: "footer-0",
-            url: "https://github.com/rodrigo-arenas",
-            className: "social-icon",
-            target: "_blank",
-            icon: <AiFillGithub size={50}/>
-        },
-        {
-            id: "footer-1",
-            url: "https://linkedin.com/in/rodrigo-arenas-gomez",
-            className: "social-icon",
-            target: "_blank",
-            icon: <FaLinkedinIn size={50}/>
-        },
-        {
-            id: "footer-2",
-            url: "https://rodrigo-arenas.medium.com",
-            className: "social-icon",
-            target: "_blank",
-            icon: <BsMedium size={50}/>
-        }
-    ]
-}
+  icons: [
+    {
+      id: "footer-0",
+      url: "https://github.com/hari31416",
+      className: "social-icon",
+      target: "_blank",
+      icon: <AiFillGithub size={50} />,
+    },
+    {
+      id: "footer-1",
+      url: "https://linkedin.com/in/hari31416",
+      className: "social-icon",
+      target: "_blank",
+      icon: <FaLinkedinIn size={50} />,
+    },
+    {
+      id: "footer-2",
+      url: "https://www.kaggle.com/hari31416",
+      className: "social-icon",
+      target: "_blank",
+      icon: <FaKaggle size={50} />,
+    },
+    {
+      id: "footer-3",
+      url: "https://goodreads.com/hari31416",
+      className: "social-icon",
+      target: "_blank",
+      icon: <FaGoodreadsG size={50} />,
+    },
+  ],
+};
 
 export default footerConfig
