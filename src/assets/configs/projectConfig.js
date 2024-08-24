a9d28c0079ddc1b818775a4af6c45e22728596ae
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-08-24 16:44:18 +0530

    update projects
    - 16 projects added
    - Notebooks and files related to some projects are added
    - shadow color for the cards is changes

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -2,149 +2,334 @@ import {BiGitRepoForked} from "react-icons/bi";
 import {AiFillGithub, AiFillEye} from "react-icons/ai";
 import {ImBook} from "react-icons/im";
 
-import sklearnGenetic from "../images/sklearn_genetic_opt.png"
-import graphEmbeddings from "../images/graph_embeddings.png"
-import kafkaMl from "../images/kafkaml.png"
-import portfolio from "../images/portfolio.png"
-
-import React from 'react'
-
+import FoodVision from "../images/food_vision.png";
+import ReVision from "../images/revision.webp";
+import NNet from "../images/nnet.png";
+import RagOptimize from "../images/ragoptimize.png";
+import CelestialClassify from "../images/celestialclassify.png";
+import BookRecommender from "../images/book_recommender.webp";
+import pystock from "../images/pystock.png";
+import optionalyzer from "../images/optionalyzer.png";
+import DeepDream from "../images/deepdream.jpg";
+import LightingProduct from "../images/lightingproduct.jpg";
+import Contrails_Identification from "../images/contrails_identification.jpg";
+import Hangman from "../images/hangman.png";
+import NLPDisasterTweets from "../images/disaster_tweets.png";
+import DigitRecognizer from "../images/MNIST.png";
+import HousePrices from "../images/house_price.jpeg";
+import SpeechRecognition from "../images/speech.png";
+import React from "react";
 const projectConfig = [
-    {
-        id: "project-5",
-        title: "Sklearn Genetic Opt",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt",
-                icon: <AiFillGithub/>,
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/fork",
-                icon: <BiGitRepoForked/>,
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/subscription",
-                icon: <AiFillEye/>,
-            },
-            {
-                name: "docs",
-                url: "https://sklearn-genetic-opt.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
-            }
-        ],
-        image: sklearnGenetic,
-        description: "scikit-learn models hyperparameters tuning and feature selection, using evolutionary algorithms.",
-        target: "_blank"
-    },
-    {
-        id: "project-4",
-        title: "Graphs Embeddings",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/Graph-Embeddings",
-                icon: <AiFillGithub/>,
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/Graph-Embeddings/fork",
-                icon: <BiGitRepoForked/>,
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/Graph-Embeddings/subscription",
-                icon: <AiFillEye/>,
-            },
-            {
-                name: "docs",
-                url: "https://graph-embeddings.readthedocs.io/",
-                icon: <ImBook/>,
-            }
-        ],
-        image: graphEmbeddings,
-        description: "Graph embeddings for downstream tasks.",
-        target: "_blank"
-    },
-    {
-        id: "project-3",
-        title: "Kafkaml Anomalies",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/subscription",
-                icon: <AiFillEye/>
-            }
-        ],
-        image: kafkaMl,
-        description: "Project for real-time anomaly detection using kafka and python.",
-        target: "_blank"
-    },
-    {
-        id: "project-2",
-        title: "Pyworkforce",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/pyworkforce",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/subscription",
-                icon: <AiFillEye/>
-            },
-            {
-                name: "docs",
-                url: "https://pyworkforce.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
-            }
-        ],
-        image: "https://miro.medium.com/max/1400/1*UDnhOFK35IbAPx15wkMgVg.jpeg",
-        description: "Standard tools for workforce management, queuing, scheduling, rostering and optimization problems.",
-        target: "_blank"
-    },
-    {
-        id: "project-1",
-        title: "Portfolio Web",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/portfolio",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/portfolio/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/portfolio/subscription",
-                icon: <AiFillEye/>
-            }
-        ],
-        image: portfolio,
-        description: "Source code of my current portfolio web page as data scientist.",
-        target: "_blank"
-    }
-]
+  {
+    id: "project-15",
+    title: "ReVision",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/ReVision",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "website",
+        url: "https://hari31416.notion.site/10a70e2c390f4e119cb709b712d3b168?v=8224d1d9652e48fd83100ab516a228d3&pvs=74",
+        icon: <AiFillEye />,
+      },
+    ],
+    image: ReVision,
+    description:
+      "Implementing various groundbreaking computer vision architecture from scratch using Tensorflow.",
+    target: "_blank",
+  },
+  {
+    id: "project-14",
+    title: "RagOptimize",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/BitwiseBrains/RagOptimize",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: RagOptimize,
+    description:
+      "Fine-Tuning Mistral-7B on financial dataset and created a RAG to answer questions on a financial dataset or article.",
+    target: "_blank",
+  },
+  {
+    id: "project-13",
+    title: "CelestialClassify",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/CelestialClassify",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: CelestialClassify,
+    description:
+      "Training machine learning models to classify celestial objects using data from the SDSS",
+    target: "_blank",
+  },
+  {
+    id: "project-12",
+    title: "Book Recommender",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/book_recommender",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/book_recommender/fork",
+        icon: <BiGitRepoForked />,
+      },
+    ],
+    image: BookRecommender,
+    description:
+      "Building a book recommendation system using collaborative filtering, content-based filtering and matrix factorization.",
+    target: "_blank",
+  },
+  {
+    id: "project-11",
+    title: "pystock",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/pystock",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/pystock/fork",
+        icon: <BiGitRepoForked />,
+      },
+      {
+        name: "docs",
+        url: "https://hari31416.github.io/pystock/html/index.html",
+        icon: <ImBook />,
+      },
+    ],
+    image: pystock,
+    description:
+      "Creating a library for portfolio optimization using models like CAPM, SIM, FF3, and FF5",
+    target: "_blank",
+  },
+  {
+    id: "project-19",
+    title: "NNet",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/NNet",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/NNet/fork",
+        icon: <BiGitRepoForked />,
+      },
+    ],
+    image: NNet,
+    description:
+      "A module for constructing and optimizing an arbitrary neural network architecture using Python and NumPy, with backpropagation and gradient descent.",
+    target: "_blank",
+  },
+  {
+    id: "project-9",
+    title: "LightingProduct",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/LightingProduct",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/LightingProduct/fork",
+        icon: <BiGitRepoForked />,
+      },
+      {
+        name: "website",
+        url: "https://huggingface.co/spaces/hari31416/LightingProduct",
+        icon: <AiFillEye />,
+      },
+      {
+        name: "docs",
+        url: "https://github.com/Hari31416/LightingProduct/blob/main/README.md",
+        icon: <ImBook />,
+      },
+    ],
+    image: LightingProduct,
+    description:
+      "Parsing PDFs, extracting product information and creating an NLP model to predict whether a product is a lighting product or not.",
+    target: "_blank",
+  },
+  {
+    id: "project-8",
+    title: "FoodVision",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Portfolio/tree/main/projects/Food%20Vision",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "website",
+        url: "https://huggingface.co/spaces/hari31416/FoodVision",
+        icon: <AiFillEye />,
+      },
+    ],
+    image: FoodVision,
+    description:
+      "Using Transfer Learning with EfficientNet to classify 101 types food images.",
+    target: "_blank",
+  },
+  {
+    id: "project-6",
+    title: "optionalyzer",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/optionalyzer",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/optionalyzer/fork",
+        icon: <BiGitRepoForked />,
+      },
+      {
+        name: "docs",
+        url: "https://github.com/Hari31416/optionalyzer/blob/main/Working_with_optionalyzer.ipynb",
+        icon: <ImBook />,
+      },
+    ],
+    image: optionalyzer,
+    description:
+      "Creating a library for option pricing and greeks calculation using Black-Scholes model. Can be used for building option trading strategies.",
+    target: "_blank",
+  },
+  {
+    id: "project-6",
+    title: "NLP With Disaster Tweets",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Portfolio/tree/main/projects/Disaster_Tweets",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: NLPDisasterTweets,
+    description: "Implementing Google's DeepDream algorithm using PyTorch.",
+    target: "_blank",
+  },
+  {
+    id: "project-5",
+    title: "DeepDream",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/DeepDream",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/DeepDream/fork",
+        icon: <BiGitRepoForked />,
+      },
+    ],
+    image: DeepDream,
+    description: "Implementing Google's DeepDream algorithm using PyTorch.",
+    target: "_blank",
+  },
+  {
+    id: "project-4",
+    title: "Contrails Identification",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Identify_Contrails",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/Identify_Contrails/fork",
+        icon: <BiGitRepoForked />,
+      },
+    ],
+    image: Contrails_Identification,
+    description:
+      "Training ML models to identify contrails in satellite images using image segmentation.",
+    target: "_blank",
+  },
+  {
+    id: "project-3",
+    title: "Hangman",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/hangman",
+        icon: <AiFillGithub />,
+      },
+      {
+        name: "fork",
+        url: "https://github.com/Hari31416/hangman/fork",
+        icon: <BiGitRepoForked />,
+      },
+      {
+        name: "docs",
+        url: "https://github.com/Hari31416/hangman/blob/main/algorithm.md",
+        icon: <ImBook />,
+      },
+    ],
+    image: Hangman,
+    description:
+      "Developing an advanced Hangman solver algorithm using ngram models with NLTK.",
+    target: "_blank",
+  },
+  {
+    id: "project-2",
+    title: "Digit Recognizer",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Portfolio/tree/main/projects/MNIST_Digits",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: DigitRecognizer,
+    description: "Training a CNN model to recognize handwritten digits.",
+    target: "_blank",
+  },
+  {
+    id: "project-1",
+    title: "House Prices Prediction",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Portfolio/tree/main/projects/Housing_Price",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: HousePrices,
+    description: "Predicting house prices using regression models.",
+    target: "_blank",
+  },
+  {
+    id: "project-0",
+    title: "TensorFlow Speech Recognition Challenge",
+    links: [
+      {
+        name: "repo",
+        url: "https://github.com/Hari31416/Portfolio/tree/main/projects/TensorFlow_Speech_Recognition",
+        icon: <AiFillGithub />,
+      },
+    ],
+    image: SpeechRecognition,
+    description: "Training a nueral networks to recognize spoken words.",
+    target: "_blank",
+  },
+];
 
 export default projectConfig
